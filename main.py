f30604fdba7db5f03a27f9ef5e206916f0c3e0d7
WheatleyHDD <wheatleyhdd@gmail.com>
2022-11-26 12:56:27 +0500

    Тест конфликтов

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     prog = input("Что делаем (Вакансии/Статистика): ")
     if prog == "Вакансии": vacancies.InputConnect()
     elif prog == "Статистика": elearn_2_1_3.InputConnect()
-    else: print("Неверная программа")
+    else: print("Неизвестная программа")
