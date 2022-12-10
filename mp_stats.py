2e2abddf8ca0f6c5f030c6fe2c4b1eee5ee23905
WheatleyHDD <wheatleyhdd@gmail.com>
2022-12-11 00:20:07 +0500

    Мультипоток попытка н 1

--- mp_stats.py
+++ mp_stats.py
@@ -1,5 +1,8 @@
 import csv
-from datetime import datetime
+import os
+import multiprocessing
+from multiprocessing.managers import BaseManager
+
 import openpyxl
 import openpyxl.utils
 import openpyxl.styles
@@ -33,14 +36,6 @@ class Vacancy:
         """Конструктор объекта вакансий
 
         :param dict vacancy: Словарь вакансии
-
-        >>> Vacancy({'test_data': True}).salary_average
-        909.9
-        >>> type(Vacancy({'test_data': True})).__name__
-        'Vacancy'
-        >>> Vacancy({'test_data': True}).year
-        2007
-
         """
         if 'test_data' in vacancy.keys():
             vacancy = {
@@ -64,10 +59,74 @@ class Vacancy:
         self.salary_currency = vacancy['salary_currency']
         self.salary_average = self.currency_to_rub[self.salary_currency] * (self.salary_from + self.salary_to) / 2
         self.area_name = vacancy['area_name']
-        # Ранняя имплементация
-        # self.year = int(datetime.strptime(vacancy['published_at'], '%Y-%m-%dT%H:%M:%S%z').year)
-        # Новая
         self.year = int(vacancy['published_at'][:4])
+
+
+class StatsContainer:
+    """Класс для объединения объектов статистики
+
+    Attributes:
+        stats (list): Объекты статистики
+    """
+
+    def __init__(self):
+        self.stats = []
+        self.vacancies_number = {}
+        self.salary_dict = {}
+
+    def write(self, stat_list):
+        self.stats = stat_list
+
+    def get_stat1(self):
+        """ Получить динамику уровня зарплат по годам
+
+        :return dict: Динамика уровня зарплат по годам
+        """
+        for o in self.stats:
+            self.salary_dict.update(o.salary)
+        result = {}
+        for year, sal in self.salary_dict.items():
+            result[year] = int(sum(sal) / len(sal))
+        return result
+
+    def get_stat2(self):
+        """Получить динамику количества вакансий по годам
+
+        :return dict: Динамика количества вакансий по годам
+        """
+        for o in self.stats: self.vacancies_number.update(o.vacancies_number)
+        return self.vacancies_number
+
+    def get_stat3(self):
+        """Получить динамику уровня зарплат по годам для выбранной профессии
+
+        :return dict: Динамика уровня зарплат по годам для выбранной профессии
+        """
+        vac_sal = {}
+        for o in self.stats: vac_sal.update(o.salary_of_vacancy_name)
+        result = {}
+        for year, sal in vac_sal.items():
+            if len(sal) == 0:
+                result[year] = 0
+            else:
+                result[year] = int(sum(sal) / len(sal))
+        return result
+
+    def get_stat4(self):
+        """Получить динамику количества вакансий по годам для выбранной профессии
+
+        :return dict: Динамика количества вакансий по годам для выбранной профессии
+        """
+        vac_count_sal = {}
+        for o in self.stats: vac_count_sal.update(o.vac_count_of_vacancy_name)
+        return vac_count_sal
+
+    def print_statistics(self):
+        """Вывести статистику в консоль"""
+        print('Динамика уровня зарплат по годам: ' + str(self.get_stat1()))
+        print('Динамика количества вакансий по годам: ' + str(self.get_stat2()))
+        print('Динамика уровня зарплат по годам для выбранной профессии: ' + str(self.get_stat3()))
+        print('Динамика количества вакансий по годам для выбранной профессии: ' + str(self.get_stat4()))
 
 
 class Statistic:
@@ -78,18 +137,15 @@ class Statistic:
         vacancies_number (dict): Количество вакансий по названиям
         salary_of_vacancy_name (dict): Зарплата по вакансиям
         vac_count_of_vacancy_name (dict): Количество вакансий по названию
-        salary_city (dict): Вакансии по городам
-        vac_city_number (dict): Количество вакансий по городам
         count_of_vacancies (int): Количество вакансий
     """
+
     def __init__(self):
         """Конструктор класса статистики"""
         self.salary = {}
         self.vacancies_number = {}
         self.salary_of_vacancy_name = {}
         self.vac_count_of_vacancy_name = {}
-        self.salary_city = {}
-        self.vac_city_number = {}
         self.count_of_vacancies = 0
 
     def year_sal_dynamics(self, vacancy):
@@ -99,7 +155,8 @@ class Statistic:
         """
         if vacancy.year not in self.salary:
             self.salary[vacancy.year] = [vacancy.salary_average]
-        else: self.salary[vacancy.year].append(vacancy.salary_average)
+        else:
+            self.salary[vacancy.year].append(vacancy.salary_average)
 
     def year_vac_dynamics(self, vacancy):
         """Составление динамики вакансий по годам
@@ -108,7 +165,8 @@ class Statistic:
         """
         if vacancy.year not in self.vacancies_number:
             self.vacancies_number[vacancy.year] = 1
-        else: self.vacancies_number[vacancy.year] += 1
+        else:
+            self.vacancies_number[vacancy.year] += 1
 
     def curr_vac_dynamics(self, vacancy, vacancy_name):
         """Составление динамики зарплаты по конкретной вакансии по городам
@@ -119,29 +177,21 @@ class Statistic:
         if vacancy.name.find(vacancy_name) != -1:
             if vacancy.year not in self.salary_of_vacancy_name:
                 self.salary_of_vacancy_name[vacancy.year] = [vacancy.salary_average]
-            else: self.salary_of_vacancy_name[vacancy.year].append(vacancy.salary_average)
-
+            else:
+                self.salary_of_vacancy_name[vacancy.year].append(vacancy.salary_average)
             if vacancy.year not in self.vac_count_of_vacancy_name:
                 self.vac_count_of_vacancy_name[vacancy.year] = 1
-            else: self.vac_count_of_vacancy_name[vacancy.year] += 1
+            else:
+                self.vac_count_of_vacancy_name[vacancy.year] += 1
 
-    def city_sal_dynamics(self, vacancy):
-        """Составление динамики зарплаты по городам
+        if not self.salary_of_vacancy_name:
+            self.salary_of_vacancy_name = self.salary.copy()
+            self.salary_of_vacancy_name = dict([(key, []) for key, value in self.salary_of_vacancy_name.items()])
 
-        :param Vacancy vacancy: Вакансия
-        """
-        if vacancy.area_name not in self.salary_city:
-            self.salary_city[vacancy.area_name] = [vacancy.salary_average]
-        else: self.salary_city[vacancy.area_name].append(vacancy.salary_average)
-
-    def city_count_dynamics(self, vacancy):
-        """Составление динамики количества вакансий по городам
-
-        :param Vacancy vacancy: Вакансия
-        """
-        if vacancy.area_name not in self.vac_city_number:
-            self.vac_city_number[vacancy.area_name] = 1
-        else: self.vac_city_number[vacancy.area_name] += 1
+        if not self.vac_count_of_vacancy_name:
+            self.vac_count_of_vacancy_name = self.vacancies_number.copy()
+            self.vac_count_of_vacancy_name = dict(
+                [(key, 0) for key, value in self.vac_count_of_vacancy_name.items()])
 
     def write(self, vacancy, vacancy_name):
         """Заполение статистики
@@ -152,82 +202,67 @@ class Statistic:
         self.year_sal_dynamics(vacancy)
         self.year_vac_dynamics(vacancy)
         self.curr_vac_dynamics(vacancy, vacancy_name)
+        self.count_of_vacancies += 1
+
+
+class CityStats:
+    def __init__(self):
+        self.salary_city = {}
+        self.vac_city_number = {}
+        self.count_of_vacancies = 0
+
+        self.result1 = {}
+        self.result2 = {}
+
+    def city_sal_dynamics(self, vacancy):
+        """Составление динамики зарплаты по городам
+
+        :param Vacancy vacancy: Вакансия
+        """
+        if vacancy.area_name not in self.salary_city:
+            self.salary_city[vacancy.area_name] = [vacancy.salary_average]
+        else:
+            self.salary_city[vacancy.area_name].append(vacancy.salary_average)
+
+    def city_count_dynamics(self, vacancy):
+        """Составление динамики количества вакансий по городам
+
+        :param Vacancy vacancy: Вакансия
+        """
+        if vacancy.area_name not in self.vac_city_number:
+            self.vac_city_number[vacancy.area_name] = 1
+        else:
+            self.vac_city_number[vacancy.area_name] += 1
+
+    def write(self, vacancy):
         self.city_sal_dynamics(vacancy)
         self.city_count_dynamics(vacancy)
         self.count_of_vacancies += 1
-
-    def get_stat1(self):
-        """ Получить динамику уровня зарплат по годам
-
-        :return dict: Динамика уровня зарплат по годам
-        """
-        result = {}
-        for year, sal in self.salary.items():
-            result[year] = int(sum(sal) / len(sal))
-        return result
-
-    def get_stat2(self):
-        """Получить динамику количества вакансий по годам
-
-        :return dict: Динамика количества вакансий по годам
-        """
-        return self.vacancies_number
-
-    def get_stat3(self):
-        """Получить динамику уровня зарплат по годам для выбранной профессии
-
-        :return dict: Динамика уровня зарплат по годам для выбранной профессии
-        """
-        if not self.salary_of_vacancy_name:
-            self.salary_of_vacancy_name = self.salary.copy()
-            self.salary_of_vacancy_name = dict([(key, []) for key, value in self.salary_of_vacancy_name.items()])
-        result = {}
-        for year, sal in self.salary_of_vacancy_name.items():
-            if len(sal) == 0: result[year] = 0
-            else: result[year] = int(sum(sal) / len(sal))
-        return result
-
-    def get_stat4(self):
-        """Получить динамику количества вакансий по годам для выбранной профессии
-
-        :return dict: Динамика количества вакансий по годам для выбранной профессии
-        """
-        if not self.vac_count_of_vacancy_name:
-            self.vac_count_of_vacancy_name = self.vacancies_number.copy()
-            self.vac_count_of_vacancy_name = dict(
-                [(key, 0) for key, value in self.vac_count_of_vacancy_name.items()])
-        return self.vac_count_of_vacancy_name
 
     def get_stat5and6(self):
         """Получить уровень и долю зарплат по городам (в порядке убывания)
 
         :return: Уровень и доля зарплат по городам (в порядке убывания)
         """
-        result1 = {}
+        self.result1 = {}
         for city, count in self.vac_city_number.items():
-            result1[city] = round(count / self.count_of_vacancies, 4)
+            self.result1[city] = round(count / self.count_of_vacancies, 4)
         result1 = list(filter(lambda a: a[-1] >= 0.01,
-                             [(key, value) for key, value in result1.items()]))
+                              [(key, value) for key, value in self.result1.items()]))
         result1.sort(key=lambda a: a[-1], reverse=True)
 
-        result2 = {}
+        self.result2 = {}
         for city, sal in self.salary_city.items():
-            result2[city] = int(sum(sal) / len(sal))
+            self.result2[city] = int(sum(sal) / len(sal))
         result2 = list(filter(lambda a: a[0] in list(dict(result1).keys()),
-                             [(key, value) for key, value in result2.items()]))
+                              [(key, value) for key, value in self.result2.items()]))
         result2.sort(key=lambda a: a[-1], reverse=True)
 
         return dict(result2[:10]), dict(result1[:10])
 
-    def print_statistics(self):
-        """Вывести статистику в консоль"""
-        print('Динамика уровня зарплат по годам: ' + str(self.get_stat1()))
-        print('Динамика количества вакансий по годам: ' + str(self.get_stat2()))
-        print('Динамика уровня зарплат по годам для выбранной профессии: ' + str(self.get_stat3()))
-        print('Динамика количества вакансий по годам для выбранной профессии: ' + str(self.get_stat4()))
-        stat5, stat6 = self.get_stat5and6()
-        print('Уровень зарплат по городам (в порядке убывания): ' + str(stat5))
-        print('Доля вакансий по городам (в порядке убывания): ' + str(stat6))
+    def print_statistic(self):
+        print('Уровень зарплат по городам (в порядке убывания): ' + str(self.result2))
+        print('Доля вакансий по городам (в порядке убывания): ' + str(self.result1))
 
 
 class DataSet:
@@ -236,6 +271,7 @@ class DataSet:
         file_name (str): Название файла
         vacancy_name (str): Название необходимой вакансии
     """
+
     def __init__(self, file_name, vacancy_name):
         """Конструктор класса DataSet
 
@@ -244,6 +280,7 @@ class DataSet:
         """
         self.file_name = file_name
         self.vacancy_name = vacancy_name
+        self.data = []
 
     def csv_reader(self):
         """Читает CSV файл"""
@@ -253,7 +290,7 @@ class DataSet:
             header_length = len(header)
             for row in reader:
                 if '' not in row and len(row) == header_length:
-                    yield dict(zip(header, row))
+                    self.data.append(Vacancy(dict(zip(header, row))))
 
     def get_statistic(self):
         """Получить статистические данные
@@ -262,11 +299,14 @@ class DataSet:
         """
         statistics = Statistic()
 
-        for vacancy_dictionary in self.csv_reader():
-            vacancy = Vacancy(vacancy_dictionary)
+        for vacancy in self.data:
             statistics.write(vacancy, self.vacancy_name)
 
         return statistics
+
+
+class CustomManager(BaseManager):
+    pass
 
 
 class InputConnect:
@@ -276,6 +316,7 @@ class InputConnect:
         file_name (str): Название файла
         vacancy_name (list): Название необходимой вакансии
     """
+
     def __init__(self, fn=None, vn=None):
         """
         Начало работы программы
@@ -287,17 +328,29 @@ class InputConnect:
         if vn is None:
             self.vacancy_name = input('Введите название профессии: ')
 
-        dataset = DataSet(self.file_name, self.vacancy_name)
-        stats = dataset.get_statistic()
-        stats.print_statistics()
+        container = StatsContainer()
+        with multiprocessing.Manager() as mngr:
+            statistic_list = mngr.list()
 
-        stat5, stat6 = stats.get_stat5and6()
-        report = Report(self.vacancy_name, stats.get_stat1(), stats.get_stat2(),
-                        stats.get_stat3(), stats.get_stat4(), stat5, stat6)
+            files = [self.file_name + "/" + f for f in os.listdir(self.file_name)]
+            pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+            [pool.apply(self.generate_statistic, args=(f, statistic_list)) for f in files]
 
-        report.generate_excel('report.xlsx')
-        report.generate_img('graph.png')
-        report.generate_pdf('report.pdf')
+            container.write(statistic_list)
+
+            container.print_statistics()
+
+            report = Report(self.vacancy_name, container.get_stat1(), container.get_stat2(), container.get_stat3(),
+                            container.get_stat4(), {}, {})
+
+            report.generate_excel('report.xlsx')
+            report.generate_img('graph.png')
+            report.generate_pdf('report.pdf')
+
+    def generate_statistic(self, queue, statistic_list):
+        dataset = DataSet(queue, self.vacancy_name)
+        dataset.csv_reader()
+        statistic_list.append(dataset.get_statistic())
 
 
 class Report:
@@ -314,6 +367,7 @@ class Report:
         stats5 (dict): Уровень зарплат по городам (в порядке убывания)
         stats6 (dict): Доля зарплат по городам (в порядке убывания)
     """
+
     def __init__(self, vacancy_name, stats1, stats2, stats3, stats4, stats5, stats6):
         """Конструктор класса для построения репортов
 
@@ -343,17 +397,20 @@ class Report:
         # Лист "Статистика по годам"
         ws1 = self.wb.active
         ws1.title = 'Статистика по годам'
-        ws1.append(['Год', 'Средняя зарплата', 'Средняя зарплата - ' + self.vacancy_name, 'Количество вакансий', 'Количество вакансий - ' + self.vacancy_name])
+        ws1.append(['Год', 'Средняя зарплата', 'Средняя зарплата - ' + self.vacancy_name, 'Количество вакансий',
+                    'Количество вакансий - ' + self.vacancy_name])
         for year in self.stats1.keys():
             ws1.append([year, self.stats1[year], self.stats3[year], self.stats2[year], self.stats4[year]])
 
-        data = [['Год ', 'Средняя зарплата ', ' Средняя зарплата - ' + self.vacancy_name, ' Количество вакансий', ' Количество вакансий - ' + self.vacancy_name]]
+        data = [['Год ', 'Средняя зарплата ', ' Средняя зарплата - ' + self.vacancy_name, ' Количество вакансий',
+                 ' Количество вакансий - ' + self.vacancy_name]]
         column_widths = []
         for row in data:
             for i, cell in enumerate(row):
                 if len(column_widths) > i:
                     if len(cell) > column_widths[i]: column_widths[i] = len(cell)
-                else: column_widths += [len(cell)]
+                else:
+                    column_widths += [len(cell)]
 
         for i, column_width in enumerate(column_widths, 1):  # ,1 to start at 1
             ws1.column_dimensions[openpyxl.utils.get_column_letter(i)].width = column_width + 2
@@ -372,7 +429,8 @@ class Report:
                 cell = str(cell)
                 if len(column_widths) > i:
                     if len(cell) > column_widths[i]: column_widths[i] = len(cell)
-                else: column_widths += [len(cell)]
+                else:
+                    column_widths += [len(cell)]
 
         for i, column_width in enumerate(column_widths, 1):  # ,1 to start at 1
             ws2.column_dimensions[openpyxl.utils.get_column_letter(i)].width = column_width + 2
--- profiler.py
+++ profiler.py
@@ -1,11 +1,16 @@
 import main as mainProgram
 import cProfile
+import vacancies
+import statistics
+import year_splitter
+import mp_stats
 
 if __name__ == '__main__':
     pr = cProfile.Profile()
     pr.enable()
 
-    mainProgram.start()
+    # mp_stats.InputConnect("chunks", "Аналитик")
+    statistics.InputConnect("vacancies.csv", "Аналитик")
 
     pr.disable()
     pr.print_stats(sort="tottime")
